16f2532b247add32cc2cb75c9f3c503265dbdc2f
Pranav <pranav@myrdd.info>
2022-10-10 23:04:46 -0700

    Initial Commit with Basic Cargo setup

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use std::io;
+
+fn main() {
+    println!("Hello, world!");
+
+    let mut x = Int::new();
+
+}                             
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
